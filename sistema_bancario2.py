7aebd901e6a2b320f2b66c39e0c68c70104e57dd
Danilo <danilodecarvalho1808@gmail.com>
2025-03-19 18:17:06 -0300

    commit

--- sistema_bancario2.py
+++ sistema_bancario2.py
@@ -1,12 +1,9 @@
-#projeto bancário - Sintaxe Básica
-#depósito, saque e extrato
-#apenas um usuário, sem identificação
-#todos os depósitos devem ser armazenados em extrato
-#pode ser feito apenas 3 saques diários, com 500 reais cada
-#caso o usuário nao tenha, uma mensagem informando que nao é possivel sacar mais que o saque deve ser mostrada
-#todos os saques devem aparecer em extrato
-#o extrato deve listar todos os depoistos e saques e o saldo atual da conta
-#formato é R$ xxxx.xx
+#mesmo sistema bancário anterior, mas melhorado
+#1. limite de 10 transações diárias
+#2. se o usuário tentar fazer uma transação após atingir o limite, deve ser informado que ele atingiu esse
+#3. mostre, no extrato, a data e hora de todas as transações
+
+from datetime import datetime
 
 operacoes_possiveis = """ 
 
@@ -21,13 +18,19 @@ operacoes_possiveis = """
 extrato = []
 limites_saques = 3
 saldo_atual = 1500.00
+limites_transacoes = 10
+hora_formatada = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
 while True:
     operacao_escolhida = input("\n" + f'Olá, qual operação você gostaria de realizar hoje? {operacoes_possiveis}')
+    if limites_transacoes == 0 and (operacao_escolhida== "1" or operacao_escolhida=="2"):
+        print("Você atingiu o número máximo de transações por hoje.")
+        continue
     if operacao_escolhida == "1":
         deposito = float(input("\n" + "Quanto você gostaria de depositar? \n"))
         saldo_atual = saldo_atual + deposito
-        extrato.append(f"Depósito de R${deposito:.2f} reais realizado. \n")
+        extrato.append(f"Depósito de R${deposito:.2f} reais realizado em {hora_formatada} \n")
+        limites_transacoes = limites_transacoes - 1
         print(f"Depósito de R${deposito:.2f} realizado com sucesso.")
     elif operacao_escolhida == "2":
         if limites_saques == 0:
@@ -41,7 +44,8 @@ while True:
             print("Você não pode sacar um valor maior que o seu saldo atual.")
             continue
         saldo_atual = saldo_atual - saque
-        extrato.append(f"Saque de R${saque:.2f} realizado. \n")
+        extrato.append(f"Saque de R${saque:.2f} realizado em {hora_formatada} \n")
+        limites_transacoes = limites_transacoes - 1
         print(f"Saque de R${saque:.2f} realizado com sucesso.")
         limites_saques = limites_saques - 1
     elif operacao_escolhida == "3":
